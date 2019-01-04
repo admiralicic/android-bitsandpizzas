afe8bf6e03cc8833934d942bcc8c1ca51c1142de
Admir Alicic <admir.alicic@gmail.com>
2019-01-04 10:22:16 +0100

    Add pizza recycler view

---
+++ app/src/main/java/com/example/admir/bitsandpizzas/CaptionedImagesAdapter.java
@@ -0,0 +1,56 @@
+package com.example.admir.bitsandpizzas;
+
+import android.graphics.drawable.Drawable;
+import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class CaptionedImagesAdapter extends RecyclerView.Adapter<CaptionedImagesAdapter.ViewHolder> {
+
+    private String[] captions;
+    private int[] imageIds;
+
+    public CaptionedImagesAdapter(String[] captions, int[] imageIds) {
+        this.captions = captions;
+        this.imageIds = imageIds;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        private CardView cardView;
+
+        public ViewHolder(@NonNull CardView itemView) {
+            super(itemView);
+            cardView = itemView;
+        }
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        CardView cv = (CardView) LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.card_captioned_image, viewGroup, false);
+        return new ViewHolder(cv);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CaptionedImagesAdapter.ViewHolder viewHolder, int i) {
+        CardView cardView = viewHolder.cardView;
+        ImageView imageView = cardView.findViewById(R.id.info_image);
+        Drawable drawable = ContextCompat.getDrawable(cardView.getContext(), imageIds[i]);
+        imageView.setImageDrawable(drawable);
+        imageView.setContentDescription(captions[i]);
+        TextView textView = cardView.findViewById(R.id.info_text);
+        textView.setText(captions[i]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return captions.length;
+    }
+}
